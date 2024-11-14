11524b2738ac593489754dbcf1e287acbb193ffe
Synergy-Frontend-Dev3 <108149127+Muhammad-Shariq365@users.noreply.github.com>
2024-11-15 03:09:11 +0500

    Update services.jsx

--- moten-villas/src/components/services.jsx
+++ moten-villas/src/components/services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
   };
 
   return (
-    <div id="services" className="my-24">
+    <div id="services" className="my-12">
       <div className="container mx-auto px-4">
         
         {/* Animated "OUR SERVICES" Heading */}
@@ -23,9 +23,9 @@ const Services = () => {
           variants={headingVariants}
         >
           <div className="flex items-center justify-center">
-            <div className="border-l-4 border-orange-500 h-10 mr-4"></div>
-            <h2 className="text-black uppercase font-bold text-center">OUR SERVICES</h2>
-            <div className="border-r-4 border-orange-500 h-10 ml-4"></div>
+       <div className="border-l-4 border-orange-500 h-10 mr-4"></div>
+          <h2 className="text-[clamp(2rem,5vw,3rem)] font-extrabold text-black uppercase">OUR SERVICES</h2>
+          <div className="border-r-4 border-orange-500 h-10 ml-4"></div>
           </div>
         </motion.h2>
 
